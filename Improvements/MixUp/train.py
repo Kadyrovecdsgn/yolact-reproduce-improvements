c1bf342e8c18bd82455c96fc07a4574ac5d9d1cb
Daniil Botvenko <134534485+Kadyrovecdsgn@users.noreply.github.com>
2026-06-04 15:23:35 +0800

    Update train.py

--- Improvements/MixUp/train.py
+++ Improvements/MixUp/train.py
@@ -25,6 +25,23 @@ from utils.mixup import mixup_batch
 # import torch.multiprocessing as mp
 # mp.set_start_method('spawn', force=True)
 
+def set_seed(seed=42):
+    import random
+    import numpy as np
+    import torch
+
+    random.seed(seed)
+    np.random.seed(seed)
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+set_seed(42)
+
 # Oof
 import eval as eval_script
 
